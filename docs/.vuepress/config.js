0dd4bbf405faba2c6fa0d342a5aacc7cd0fea25c
Takeyuki UEDA <ueda@latelierdueda.com>
2023-07-09 22:19:07 +0900

    add mermaid

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -20,7 +20,8 @@ module.exports = {
         'google_ad_client': 'ca-pub-1717720203982550', // ca-pub-0000000000000000
         'enable_page_level_ads': true
       }
-    ]
+    ],
+    'mermaidjs'
   
   ],
   themeConfig: {
